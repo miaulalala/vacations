457e580a6f1b202cd27da0464df328aedb40f640
Anna Larch <anna@nextcloud.com>
2023-09-21 18:31:58 +0200

    Initial commit
    Signed-off-by: Anna Larch <anna@nextcloud.com>

---
+++ lib/Service/NoteNotFound.php
@@ -0,0 +1,9 @@
+<?php
+declare(strict_types=1);
+// SPDX-FileCopyrightText: Anna Larch <anna.larch@gmx.net>
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+namespace OCA\Vacation\Service;
+
+class NoteNotFound extends \Exception {
+}
---
+++ tests/Unit/Controller/NoteApiControllerTest.php
@@ -0,0 +1,15 @@
+<?php
+declare(strict_types=1);
+// SPDX-FileCopyrightText: Anna Larch <anna.larch@gmx.net>
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+namespace OCA\Vacation\Tests\Unit\Controller;
+
+use OCA\Vacation\Controller\NoteApiController;
+
+class NoteApiControllerTest extends NoteControllerTest {
+	public function setUp(): void {
+		parent::setUp();
+		$this->controller = new NoteApiController($this->request, $this->service, $this->userId);
+	}
+}
